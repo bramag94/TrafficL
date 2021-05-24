bb7d3db6b827ee0bf8063b859b6e43e0249d9489
bramag94 <bramag94@hotmail.com>
2021-05-24 17:44:29 +0000

    base HTML

---
+++ src/js/component/traffic.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+export function TrafficLight() {
+	return (
+		<div className="container text-center mt-5">
+			<div className="bodyt">
+				<div>
+					<div className="circle"></div>
+				</div>
+				<div>
+					<div className="circle"></div>
+				</div>
+				<div>
+					<div className="circle"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
