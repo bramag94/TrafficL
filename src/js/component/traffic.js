71bd533dbbc6f1823855279e305d3e75860298a8
bramag94 <bramag94@hotmail.com>
2021-05-24 20:58:26 +0000

    funcionalidad y use state

--- src/js/component/traffic.js
+++ src/js/component/traffic.js
@@ -1,22 +1,39 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function TrafficLight() {
+	const [clicked, setShadow] = useState(null);
+
+	console.log(setShadow);
+	const extraredclass = "";
+	if (setShadow == "red") extraredclass == "selected";
+	const extrayellowclass = "";
+	if (setShadow == "yellow") extraredclass == "selected";
+	const extragreenclass = "";
+	if (setShadow == "green") extraredclass == "selected";
+
 	return (
 		<div className="container text-center mt-5">
 			<div>
 				<div className="bodyt">
 					<div>
-						<div className="circle"></div>
+						<div
+							className={`red circle ${extraredclass}`}
+							onClick={() => setShadow({ clicked: "red" })}></div>
 					</div>
 					<div>
-						<div className="circle"></div>
+						<div
+							className={`yellow circle ${extrayellowclass}`}
+							onClick={() =>
+								setShadow({ clicked: "yellow" })
+							}></div>
 					</div>
 					<div>
-						<div className="circle"></div>
+						<div
+							className={`green circle ${extragreenclass}`}
+							onClick={() =>
+								setShadow({ clicked: "green" })
+							}></div>
 					</div>
 				</div>
 			</div>
